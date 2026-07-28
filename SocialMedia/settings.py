b9529eb49497fc6bdcebbe19577878e09507ff26
Meherab <anindya@NybSyss-MacBook-Air.local>
2026-07-28 10:29:53 +0600

    Fix chat JS bug and add mobile-responsive CSS

--- SocialMedia/settings.py
+++ SocialMedia/settings.py
@@ -136,8 +136,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+#SITE_URL="https://127.0.0.1:8000"
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'mchowdhury201212@bscse.uiu.ac.bd')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'zvbc xlix kanm vjwq')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
